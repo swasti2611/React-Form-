85e38125949df34ae495e9a62408c9f6a067373e
Swati jagtap <swatijagtap5060@gmail.com>
2024-02-26 16:46:54 +0530

    useState form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -14,12 +14,17 @@ function Form() {
   let handleEmail=(emailvalue)=>{
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const isValid=regex.test(emailvalue);
-    setEmailValid(isValid);
+    if(isValid && emailvalue!=" "){
+setEmailValid(true);
     return true;
+  }else{
+    setEmailValid(false);
+    return false;
+    }
   }
 
   const handlePassword=(passvalue)=>{
-      if(passvalue>=8){
+      if(passvalue.length>=8 && (passvalue !=" ")){
         setPasswordValid(true);
         return true;
       }else{
@@ -29,7 +34,7 @@ function Form() {
   }
 
   const handleConfirmPass=(confirmPass)=>{
-   if(confirmPass==password){
+   if(confirmPass===password && confirmPass!=" "){
     setConfirmValid(true)
     return true;
    }else{
@@ -38,7 +43,7 @@ function Form() {
   }
   function handleSubmit(event){
     event.preventDefault();
-    if(handleEmail && handlePassword && handleConfirmPass){
+    if(setEmailValid && setPasswordValid && setConfirmValid&& email && password&& confirm){
       alert("form submitted successfully")
     }else{
       alert("can’t submit the form.")
@@ -50,21 +55,21 @@ function Form() {
         <div>
            <label htmlFor='email'>Email</label>
            <input type='email' id='email' value={email} onChange={(e)=>{setEmail(e.target.value);handleEmail(e.target.value)}} style={{ border: emailValid ? '2px solid green' : '1px solid red' }}/>
-           {!emailValid && <p>invalid email format</p>}
+           {!emailValid && <p className='email-p'>invalid email format</p>}
         </div>
        
         <div>
            <label htmlFor='pass'>Password</label>
            <input type='password' id='pass' value={password} onChange={(e)=>{setPassword(e.target.value);handlePassword(e.target.value)}} style={{ border: validpassword ? '2px solid green' : '1px solid red' }}/>
-           {!validpassword && <p>Password must be at least 8 character</p>}
+           {!validpassword && <p className='password-p'>Password must be at least 8 character</p>}
         </div>
         <div>
            <label htmlFor='confirm'>confirm Password</label>
            <input type='password' id='confirm' value={confirm} onChange={(e)=>{
             setConfirm(e.target.value);
             handleConfirmPass(e.target.value);
-           }} style={{ border: validpassword ? '2px solid green' : '1px solid red' }}/>
-            {!validconfirm && <p>Password do not match</p>}
+           }} style={{ border: validconfirm ? '2px solid green' : '1px solid red' }}/>
+            {!validconfirm && <p className='confirm-p'>Password do not match</p>}
         </div>
         <div>
           <input type='submit' value='submit' id='submit'/>
